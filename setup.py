330bdea85f094363032c507e668e7809b64e9349
Folgert Karsdorp <fbkarsdorp@gmail.com>
2014-11-12 13:47:17 +0100

    updating

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ from distutils.core import setup
 setup(
     name='plm',
     version='0.0.1',
+    packages=['plm'],
     author='Folgert Karsdorp',
     author_email='fbkarsdorp AT fastmail DOT nl',
     install_requires=['numpy', 
